c17330a7d8bb6b5656e7f445d8327a24c1e8773c
yeetree <64941543+yeetree@users.noreply.github.com>
2024-08-17 12:41:19 -0500

    stuff (wip)
    asdasdsad

---
+++ Scripts/UI/planettooltip.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var shouldtooltip = false # Whether or not to show a tooltip
+var tooltipoffset: Vector2 
+
+var ui : Control # Reference to UI
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ui = get_node("../../UI")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if shouldtooltip && ui:
+		shouldtooltip = false
+		ui.get_node("Planet Label").visible = true
+		ui.get_node("Planet Label").layout.transform.position = get_parent().position
+		
+	pass
+
+
+func _on_mouse_entered():
+	print("hello")
+	shouldtooltip = true
+	pass # Replace with function body.
+
+
+func _on_mouse_exited():
+	if ui:
+		ui.get_node("Planet Tooltip").visible = false
+	pass # Replace with function body.
