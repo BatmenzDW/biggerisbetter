432668985aa914e1fe56fd8d3f285ad598a6783a
SaifBhuiya <bhuiyasaif@gmail.com>
2024-08-19 02:02:23 +0600

    Upgrade UI

--- Scripts/UI/Upgrade_UI.gd
+++ Scripts/UI/Upgrade_UI.gd
@@ -20,19 +20,40 @@ func _on_button_pressed() -> void:
 	visible = false;
 	
 
-
-func _on_buy_1_pressed() -> void:
-	planet.gain_health()
-	
 func nplanet(body):
 	planet = body
 	
 
+func _on_buy_1_pressed() -> void:
+	
+	if (Game.metal >= 2000 && Game._funds >=15000):
+		Game.spend_funds(15000)
+		Game.spend_metal(2000)
+		planet.gain_health(20)
+	else:
+		print("Not enough resources")
+	
+
 
 func _on_buy_2_pressed() -> void:
-	planet.growth += 1 
+	if (Game.oil >= 1000 && Game._funds >=25000 && Game._crystal >= 200):
+		Game.spend_funds(25000)
+		Game.spend_oil(1000)
+		Game.spend_crystal(200)
+		planet.growth += 1 
+	else:
+		print("Not enough resources")
+	
 
 
 func _on_buy_3_pressed() -> void:
-	planet.damage -= (.2 * planet.damage)
+	
+	if (Game.oil >= 10000 && Game._funds >=50000 && Game._metal >= 20000 && Game._crystal >= 250):
+		Game.spend_funds(50000)
+		Game.spend_oil(10000)
+		Game.spend_metal(20000)
+		Game.spend_crystal(250)
+		planet.damage -= (.2 * planet.damage)
+	else:
+		print("Not enough resources")
 	
