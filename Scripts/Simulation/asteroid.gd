69202a985cd5c96d31c223355a67062a309b8020
BatmenzDW <turtleterracotta@gmail.com>
2024-08-18 14:36:02 -0400

    Turrets

--- Scripts/Simulation/asteroid.gd
+++ Scripts/Simulation/asteroid.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 
 @export var earthmass := 0.1 # Unit: Mass of Earth
 
+@export var health := 10
+
 @onready var col = $CollisionShape2D
 @onready var spr = $Asteroid
 
@@ -10,6 +12,19 @@ extends RigidBody2D
 func _ready():
 	col.shape.radius = (spr.texture.get_width() + spr.texture.get_height()) / 4
 
+func get_radius() -> float:
+	return (spr.texture.get_width() + spr.texture.get_height()) / 4
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func take_damage(amount):
+	health -= amount
+	print("took " + str(amount) + " damage")
+	if health <= 0:
+		destroy()
+
+func destroy():
+	get_parent().remove_child(self)
+	queue_free()
