0b8f0f64a84360ab7bba115c8db36d8ccbaeeb6b
BatmenzDW <turtleterracotta@gmail.com>
2024-08-19 19:02:18 -0400

    Made planet trails not dependent on game speed

--- Scripts/Graphics/PlanetTrail.gd
+++ Scripts/Graphics/PlanetTrail.gd
@@ -1,7 +1,7 @@
 class_name Trail
 extends Line2D
 
-const MAX_POINTS: int = 2000
+#const MAX_POINTS: int = 2000
 @onready var curve := Curve2D.new()
 
 const MIN_DELTA : float = 0.015
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 	total_delta += delta
 	if total_delta < MIN_DELTA:
 		return
-	total_delta = 0.0
+	
 	var scalemod = Vector2(1 / get_parent().scale.x, 1 / get_parent().scale.y)
 	var radius = get_parent().orbitalRadius
 	
@@ -25,7 +25,10 @@ func _physics_process(delta: float) -> void:
 	
 	var maxpoints = 2*radius
 	
-	curve.add_point((get_parent().position * scalemod))
-	if curve.get_baked_points().size() > maxpoints:
-		curve.remove_point(0)
+	for i in range(total_delta/MIN_DELTA):
+		curve.add_point((get_parent().position * scalemod))
+		if curve.get_baked_points().size() > maxpoints:
+			curve.remove_point(0)
+	
+	total_delta -= int(total_delta/MIN_DELTA) * MIN_DELTA
 	points = curve.get_baked_points()
