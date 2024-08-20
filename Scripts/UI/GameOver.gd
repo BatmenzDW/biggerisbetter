ddb2b13c43bae92c1f00efe9d040a11ef9b5b2ee
SaifBhuiya <bhuiyasaif@gmail.com>
2024-08-20 14:47:16 +0600

    gameoverSizepicker

--- Scripts/UI/GameOver.gd
+++ Scripts/UI/GameOver.gd
@@ -8,14 +8,20 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_restart_pressed() -> void:
-	Game.start_game(self)
+	$GameOptions.visible = true
+	$VBoxContainer/CenterContainer.visible = false
 
-
+	
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_start_game_pressed():
+	Game.start_game(self, $GameOptions/SolarSystemSize.get_selected_id())
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	$GameOptions.visible = false
+	$Main.visible = true
+	pass # Replace with function body.
