aaae5edc7cf23de6fa07cc1d885db259aa63ab5b
SaifBhuiya <bhuiyasaif@gmail.com>
2024-08-20 15:24:54 +0600

    Builiding UI Tooltip

--- Scenes/UI/BuildingUi.gd
+++ Scenes/UI/BuildingUi.gd
@@ -6,6 +6,11 @@ extends Control
 func _ready() -> void:
 	SignalBus.building_selected_input.connect(_select)
 	SignalBus.building_deselected_input.connect(_deselect)
+	$MarginContainer/HBoxContainer/ItemList.set_item_tooltip(1,"Drill ($250): \n(-1) crystal/s\n(+10) oil/s")
+	$MarginContainer/HBoxContainer/ItemList.set_item_tooltip(2,"Metal Mine ($300): \n(-1) crystal/s\n(+10) metal/s")
+	$MarginContainer/HBoxContainer/ItemList.set_item_tooltip(3,"Crystal Mine ($500): \n(-1) oil/s\n(-1) metal/s\n(+5) crystal/s")
+	$MarginContainer/HBoxContainer/ItemList.set_item_tooltip(4,"Factory ($750): \n(-1) crystal/s\n(-10) oil/s\n(+1000) fund/s")
+	$MarginContainer/HBoxContainer/ItemList.set_item_tooltip(5,"Turret ($500): \n(-1) crystal per shot\n(+5) damage per shot")
 	pass # Replace with function body.
 
 
