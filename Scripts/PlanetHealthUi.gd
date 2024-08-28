475e8bb7a1ce71f10ea079c14d27fc7563240102
SaifBhuiya <bhuiyasaif@gmail.com>
2024-08-28 23:14:36 +0600

    Update PlanetHealthUi.gd

--- Scripts/PlanetHealthUi.gd
+++ Scripts/PlanetHealthUi.gd
@@ -2,12 +2,25 @@ extends Control
 @onready var SolarSytemNode = get_parent().get_parent().get_node("SolarSystem")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(get_parent().get_parent().get_node("SolarSystemGenerator").numplanet )
-	#$GridContainer.columns = get_parent().get_node("SolarSystemGenerator").numplanet
-	#print($GridContainer.columns)
-	#print(get_parent().get_parent().get_node("SolarSystem").get_child_count())
+	
+	init_health()
 
-	for i in range (0,get_parent().get_parent().get_node("SolarSystemGenerator").numplanet ):
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	for x in range (0, $GridContainer.get_child_count()):
+		if(SolarSytemNode.get_child_count() < $GridContainer.get_child_count()):
+			init_health()
+			break
+		$GridContainer.get_child(x).get_child(0).value = SolarSytemNode.get_child(x).planetHealth
+
+func init_health():
+	if($GridContainer.get_child_count()>0):
+		for x in range (0, $GridContainer.get_child_count()):
+			$GridContainer.get_child(x).queue_free()
+	
+	for i in range (0,SolarSytemNode.get_child_count()):
 		
 		var parent = Panel.new()
 		var child = ProgressBar.new()
@@ -21,12 +34,5 @@ func _ready() -> void:
 		var style = StyleBoxFlat.new()
 		style.bg_color = Color(0.7, 0.0, 0.02)
 		child.add_theme_stylebox_override("fill", style)
-		#print(get_parent().get_parent().get_node("SolarSystem").get_child(i).planetName)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print($GridContainer.get_child_count())
-	for x in range (0, $GridContainer.get_child_count()):
-		$GridContainer.get_child(x).get_child(0).value = SolarSytemNode.get_child(x).planetHealth
+	
 	
